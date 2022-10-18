d4ab00c15800fa4acf9e5c3f9d162b080bf41b26
coide-SaltedFish <15368280291l@gmail.com>
2022-10-18 22:34:13 +0800

    Initial commit

---
+++ src/main/kotlin/com/sereinfish/catcat/bot/catcatbot/CatCatBotApplication.kt
@@ -0,0 +1,11 @@
+package com.sereinfish.catcat.bot.catcatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatCatBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<CatCatBotApplication>(*args)
+}
